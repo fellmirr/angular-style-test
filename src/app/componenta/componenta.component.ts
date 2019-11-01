0c9346ecce239d4705b878cb7ecf4d6de513a322
fellmirr <account@harnes.me>
2019-11-01 11:08:41 +0100

    Initial commit

---
+++ src/app/componenta/componenta.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'componenta',
+  templateUrl: './componenta.component.html',
+  styleUrls: ['./componenta.component.css','../css/button.scss']
+})
+export class ComponentaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
---
+++ src/app/componentb/componentb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'componentb',
+  templateUrl: './componentb.component.html',
+  styleUrls: ['./componentb.component.css', '../css/button.scss']
+})
+export class ComponentbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
